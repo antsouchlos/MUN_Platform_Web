c2df1c2aa7b864788988279f454fc4a88030649f
antsouchlos <an.tsouchlos@gmail.com>
2016-08-29 10:42:47 +0300

    added upload and download of resolutions

--- public/resView.js
+++ public/resView.js
@@ -1,16 +1,38 @@
+function getName(itemValue) {
+    var result = "";
+
+    var writing = false;
+
+    for (i = 0; i < itemValue.length; i++) {
+        if (!writing) {
+            if (itemValue.charAt(i) == ':') {
+                i++;
+                writing = true;
+            }
+        } else {
+            result += itemValue.charAt(i);
+        }
+    }
+
+    return result;
+}
+
 function download() {
     var list = document.getElementById("resList");
-    
+
     //make sure something is selected
     if (list.selectedIndex != -1) {
         //get selected list item
         var selectedItem = list.options[list.selectedIndex].text;
-        
-        //set up the firebase references
+
+        //get just the resolution's name from the text of the item
+        var resName = getName(selectedItem);
+
+        //set up the firebase reference
         var storageRef = firebase.storage().ref();
-        
+
         //download file
-        storageRef.child(selectedItem).getDownloadURL().then(function(url) {
+        storageRef.child("resolutions").child(resName).getDownloadURL().then(function(url) {
             document.location.href = url;
         }).catch(function(error) {
             alert("An error occurred while downloading file");
@@ -31,7 +53,7 @@ function changeChild(name, index) {
     var list = document.getElementById("resList");
     var item = document.createElement("option");
     item.text = "Resolution " + (index+1) + ": " + name;
-    
+
     list.remove(index);
     list.add(item, index);
 }
@@ -44,24 +66,24 @@ function removeChild(index) {
 function init() {
     //set up reference to '/resolutions'
     const resolutionRef = firebase.database().ref().child('resolutions');
-    
+
     //----synchronize items of 'resList' in realtime----
     //add an item to list when a child is added to the database
     resolutionRef.on('child_added', function(childSnapshot, prevChildKey) {
         if (prevChildKey == null)
             prevChildKey = "0";
-        
+
         addChild(childSnapshot.val(), parseInt(prevChildKey) + 1);
     });
-    
+
     //change an item's text when the value of the corresponding child is changed in the database
     resolutionRef.on('child_changed', function(childSnapshot, prevChildKey) {
         if (prevChildKey == null)
             prevChildKey = "0";
-        
+
         changeChild(childSnapshot.val(), parseInt(prevChildKey));
     });
-    
+
     //remove an item when the corresponding child is removed from the database
     resolutionRef.on('child_removed', function(oldChildSnapshot) {
         removeChild(parseInt(oldChildSnapshot.key-1));
--- public/upload.js
+++ public/upload.js
@@ -1,63 +1,83 @@
-//takes a path as an argument and returns only the filename
-function getName(path) {
-    var filename = path.replace(/^.*[\\\/]/, '')
-    return filename;
-}
-
-function upload() {
-    if (document.getElementById("fileChooser").value == "") {
-        alert("You must choose a file to upload");
-    } else if (document.getElementById("resName").value == "") {
-        alert("You must choose a name for your resolution");
-    } else {
-        //set up the firebase references
-        var path = document.getElementById("fileChooser").value;
-        
-        /*var rootRef = firebase.storage.ref();
-        var fileRef = rootRef.child(document.getElementById("resName").value);
-        */
-        alert(path);
-        /*
-        //upload the file
-        var uploadTask = fileRef.put(path);
-        
-        //make the progressbar visible
-        var progressBar = document.getElementById("progressBar");
-        var progress = document.getElementById("progress");
-
-        progressBar.style.visibility="visible";
-        progress.style.visibility="visible";
-        
-        //handle the file upload
-        uploadTask.on('state_changed', function (snapshot) {
-            //handle events
-            
-            var progress = (snapshot.bytesTransfered / snapshot.totalBytes) * 100;
-            progress.style.width = progress + '%';
-            document.getElementById("progressLabel").innerHTML = progress + '%';
-            
-        }, function(error) {
-            //handle errors
-            
-            alert("An error occurred while uploading the file");
-            
-        }, function() {
-            //handle successful uploads
-            
-        });*/
-        
-
-    }
-}
-
 function init() {
     //hide the progressbar
-    
+
     var progressBar = document.getElementById("progressBar");
     var progress = document.getElementById("progress");
 
     progressBar.style.visibility = "hidden";
-    progress.style.visibility = "hidden"; 
+    progress.style.visibility = "hidden";
+
+
+    var fileChooser = document.getElementById("fileChooser");
+    var resName = document.getElementById("resName");
+
+    //set listener on the filechooser
+    fileChooser.addEventListener('change', function(e) {
+
+	if (resName.value == "") {
+            alert("You must choose a name for the resolution");
+	} else {
+	    //make the progress bar visible
+            progressBar.style.visibility="visible";
+            progress.style.visibility="visible";
+
+            //get file
+            var file = e.target.files[0];
+
+            //create storage ref
+	    var storageRef = firebase.storage().ref("resolutions/" + resName.value);
+
+            //upload file
+            var uploadTask = storageRef.put(file);
+
+            //handle the file upload
+            uploadTask.on('state_changed',
+
+                function progress(snapshot) {
+                    var progressPerc = (snapshot.bytesTransfered / snapshot.totalBytes) * 100;
+                    progress.style.width = progressPerc + '%';
+                    document.getElementById("progressLabel").innerHTML = progressPerc + '%';
+                },
+
+                function error(err) {
+                    alert("An error occurred while uploading file");
+                },
+
+                function complete() {
+                    //set progress to 100%
+                    progress.style.width = 100 + '%';
+                    document.getElementById("progressLabel").innerHTML = '100%';
+
+                    //----add the resolution to the realtime database----
+                    var rootRef = firebase.database().ref();
+                    var counterRef = rootRef.child('counter');
+
+                    var counter;
+
+                    //boolean to make sure the code doesn't execute every time the counter changes
+                    var read = false;
+
+                    //get the value of the counter
+                    counterRef.on('value', function(snapshot) {
+                        if (!read) {
+                            read = true;
+
+                            counter = parseInt(snapshot.val()) + 1;
+
+                            //create the new resolution-reference
+                            var databaseRef = firebase.database().ref().child('resolutions').child(counter.toString());
+
+                            //set the name of the new resolution
+                            databaseRef.set(resName.value);
+
+                            //update the counter
+                            counterRef.set(counter.toString());
+                        }
+                    });
+                }
+            );
+        }
+    });
 }
 
 window.onload = init;
