c07cb41b067b541ec963d4a1e4cae6e9aa0a345e
pepsipower <dimpeppas@hotmail.gr>
2016-09-01 19:20:59 +0300

    added peppas firebase cred in comment

--- public/assets/js/site_specific/firebase_init.js
+++ public/assets/js/site_specific/firebase_init.js
@@ -1,4 +1,4 @@
-  // Initialize Firebase
+  //Andreas
   var config = {
     apiKey: "AIzaSyCgkSgD6eZrRbAXK-IArz4hhPsWkwbtpkk",
     authDomain: "dsamun-9b8bd.firebaseapp.com",
@@ -6,4 +6,14 @@
     storageBucket: "dsamun-9b8bd.appspot.com",
   };
   firebase.initializeApp(config);
+
+  //peppas
+ // var config = {
+    apiKey: "AIzaSyDcn3o128Pc9_f2hta7p6vUQeVNlKb1KSI",
+    authDomain: "dsa-mun-platform.firebaseapp.com",
+    databaseURL: "https://dsa-mun-platform.firebaseio.com",
+    storageBucket: "dsa-mun-platform.appspot.com",
+  };
+  firebase.initializeApp(config);
+    
     
