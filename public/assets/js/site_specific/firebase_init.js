7e1ad02cf12d63951a21bf9b08b8a9ba600a303a
antsouchlos <an.tsouchlos@gmail.com>
2016-09-01 11:23:39 +0300

    added files, added firebase initialization

---
+++ public/assets/js/site_specific/firebase_init.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.3.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCgkSgD6eZrRbAXK-IArz4hhPsWkwbtpkk",
+    authDomain: "dsamun-9b8bd.firebaseapp.com",
+    databaseURL: "https://dsamun-9b8bd.firebaseio.com",
+    storageBucket: "dsamun-9b8bd.appspot.com",
+  };
+  firebase.initializeApp(config);
+</script>
+    
