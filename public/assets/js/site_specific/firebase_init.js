c424227859b76a4c41974960ad1b90c6430572c7
antsouchlos <an.tsouchlos@gmail.com>
2016-09-03 11:48:00 +0300

    fixed a bug where the debate status wasn't being shown on chair.html

--- public/assets/js/site_specific/firebase_init.js
+++ public/assets/js/site_specific/firebase_init.js
@@ -1,19 +1,19 @@
 //Andreas
-/*var config = {
+var config = {
     apiKey: "AIzaSyCgkSgD6eZrRbAXK-IArz4hhPsWkwbtpkk",
     authDomain: "dsamun-9b8bd.firebaseapp.com",
     databaseURL: "https://dsamun-9b8bd.firebaseio.com",
     storageBucket: "dsamun-9b8bd.appspot.com",
 };
-firebase.initializeApp(config); */
+firebase.initializeApp(config);
 
 //peppas
-var config = {
+/*var config = {
     apiKey: "AIzaSyDcn3o128Pc9_f2hta7p6vUQeVNlKb1KSI",
     authDomain: "dsa-mun-platform.firebaseapp.com",
     databaseURL: "https://dsa-mun-platform.firebaseio.com",
     storageBucket: "dsa-mun-platform.appspot.com",
 };
-firebase.initializeApp(config);
+firebase.initializeApp(config);*/
 
     
